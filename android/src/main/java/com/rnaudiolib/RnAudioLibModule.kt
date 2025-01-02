216736e5f66e52a244cd662c4ba301ed7d83d805
Udbhav Sinha <udbhavsinha003@gmail.com>
2025-01-02 15:00:43 +0530

    chore: added import for native library and updated RnAudioLibModule

--- android/src/main/java/com/rnaudiolib/RnAudioLibModule.kt
+++ android/src/main/java/com/rnaudiolib/RnAudioLibModule.kt
@@ -1,5 +1,8 @@
 package com.rnaudiolib
 
+import android.content.Context
+import android.content.Intent
+import com.blackend.udbhav.audiolib.AudioLibraryService
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 
@@ -7,15 +10,38 @@ import com.facebook.react.module.annotations.ReactModule
 class RnAudioLibModule(reactContext: ReactApplicationContext) :
   NativeRnAudioLibSpec(reactContext) {
 
+    private val audioServiceIntent = Intent(reactContext, AudioLibraryService::class.java)
+
   override fun getName(): String {
     return NAME
   }
 
+  override fun setupQueue(context: Context?, mediaUrls: MutableList<String>?) {
+    audioServiceIntent.action = "ACTION_SETUP_QUEUE"
+    audioServiceIntent.putStringArrayListExtra("mediaUrls", mediaUrls as ArrayList<String>?)
+    reactApplicationContext.startService(audioServiceIntent)
+  }
+
+  override fun play() {
+    audioServiceIntent.action = "ACTION_PLAY"
+    reactApplicationContext.startService(audioServiceIntent)
+  }
+
+  override fun pause() {
+    audioServiceIntent.action = "ACTION_PAUSE"
+    reactApplicationContext.startService(audioServiceIntent)
+  }
+
+  override fun stop() {
+    audioServiceIntent.action = "ACTION_STOP"
+    reactApplicationContext.startService(audioServiceIntent)
+  }
+
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
-  }
+//  override fun multiply(a: Double, b: Double): Double {
+//    return a * b
+//  }
 
   companion object {
     const val NAME = "RnAudioLib"
